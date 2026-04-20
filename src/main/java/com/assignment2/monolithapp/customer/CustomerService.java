ddb39c0e76ed70ea62671950c170c50b0a9b4a05
Levine Murambi <R208076T@students.msu.ac.zw>
2026-04-20 20:39:50 +0200

    exceptions

---
+++ src/main/java/com/assignment2/monolithapp/customer/CustomerService.java
@@ -0,0 +1,34 @@
+package com.assignment2.monolithapp.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        log.info("Created customer with id={}", savedCustomer.getId());
+        return savedCustomer;
+    }
+}
